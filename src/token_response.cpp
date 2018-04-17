fedef33daafa697138a7947c8211774566610086
MCMrARM <mcmrarm@gmail.com>
2018-04-17 19:55:58 +0200

    Implement TokenErrorInfo::fromXml

--- src/token_response.cpp
+++ src/token_response.cpp
@@ -1,4 +1,5 @@
 #include <msa/token_response.h>
+#include <rapidxml.hpp>
 
 using namespace msa;
 
@@ -18,6 +19,21 @@ TokenResponse TokenResponse::fromXml(rapidxml::xml_node<char> const& data) {
 }
 
 TokenErrorInfo TokenErrorInfo::fromXml(rapidxml::xml_node<char> const& data) {
-    // TODO: Implementation
-    return TokenErrorInfo();
+    TokenErrorInfo ret;
+    rapidxml::xml_node<char>* reqStatus = data.first_node("psf:reqstatus");
+    if (reqStatus != nullptr)
+        ret.reqStatus = (unsigned int) std::atoi(reqStatus->value());
+    rapidxml::xml_node<char>* errorStatus = data.first_node("psf:errorstatus");
+    if (errorStatus != nullptr)
+        ret.errorStatus = (unsigned int) std::atoi(errorStatus->value());
+    rapidxml::xml_node<char>* flowUrl = data.first_node("psf:flowurl");
+    if (flowUrl != nullptr)
+        ret.flowUrl = flowUrl->value();
+    rapidxml::xml_node<char>* inlineAuthUrl = data.first_node("psf:inlineauthurl");
+    if (inlineAuthUrl != nullptr)
+        ret.inlineAuthUrl = inlineAuthUrl->value();
+    rapidxml::xml_node<char>* inlineEndAuthUrl = data.first_node("psf:inlineendauthurl");
+    if (inlineEndAuthUrl != nullptr)
+        ret.inlineEndAuthUrl = inlineEndAuthUrl->value();
+    return ret;
 }
